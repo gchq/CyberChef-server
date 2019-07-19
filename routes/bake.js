27ffcc4ffa727f6128f02ffa92f6115af7f5ea4c
d98762625 <d98762625@gmail.com>
2019-07-19 10:25:29 +0100

    added chef into bake route, breaks.

--- routes/bake.js
+++ routes/bake.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const chef = require("cyberchef");
 
 /* GET bake listing. */
-router.post('/', function(req, res, next) {
-  res.send('baking');
+router.post('/', async function(req, res, next) {
+  const dish = await chef.bake(req.body.input, req.body.recipe);
+
+  res.send(dish.value);
 });
 
 module.exports = router;
