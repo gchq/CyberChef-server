16c9494d6a473c6a6636a6e0eff8386cc1eed775
David Perry <d.perry@utoronto.ca>
2020-05-29 16:01:19 -0400

    Ran eslint

--- routes/operations.js
+++ routes/operations.js
@@ -7,7 +7,7 @@ import { operations } from "cyberchef/src/node/index.mjs";
  */
 router.get("/", async (req, res, next) => {
     const ret = {};
-    for (let op of operations) {
+    for (const op of operations) {
         if (op.opName) ret[op.opName] = op.args;
     }
     res.send(ret);
