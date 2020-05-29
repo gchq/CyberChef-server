abf03c07a7c1248fad2b4f8a3786e1f0d632ca4d
d98762625 <d98762625@gmail.com>
2020-05-29 16:15:09 +0100

    add GET /bake for deeplink requests

--- routes/bake.js
+++ routes/bake.js
@@ -33,4 +33,38 @@ router.post("/", async function bakePost(req, res, next) {
     }
 });
 
+
+router.get("/", async function bakeGet(req, res, next) {
+    try {
+
+        if (!req.query.input) {
+            throw new TypeError("'input' query parameter is required");
+        }
+
+        if (!req.query.recipe) {
+            throw new TypeError("'recipe' query parameter is required");
+        }
+
+        // base64 decode the input
+        const inputBuffer = Buffer.from(req.query.input, "base64");
+
+        const dish = await bake(inputBuffer, req.query.recipe);
+
+        // If the dish output is binary data, base64 encode it to send
+        // over the wire.
+        let encodedDishValue;
+        if (dish.type === Dish.ARRAY_BUFFER) {
+            encodedDishValue = Buffer.from(dish.value).toString("base64");
+        }
+
+        res.send({
+            value: encodedDishValue || dish.value,
+            type: Dish.enumLookup(dish.type),
+        });
+
+    } catch (e) {
+        next(e);
+    }
+});
+
 export default router;
